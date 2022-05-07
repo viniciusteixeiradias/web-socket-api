80354a303f5734dc445fecec0a900eb9531a29fd
viniciusteixeiradias <viniciustdias2001@gmail.com>
2022-05-07 16:03:19 -0300

    Funcionando

--- app-ws.js
+++ app-ws.js
@@ -9,18 +9,61 @@ function onMessage(ws, data) {
   ws.send(`recebido!`);
 }
  
-function onConnection(ws, req) {
-  ws.on('message', data => onMessage(ws, data));
-  ws.on('error', error => onError(ws, error));
-  console.log(`onConnection`);
-}
+// function onConnection(ws, req) {
+//   ws.on('message', data => onMessage(ws, data));
+//   ws.on('error', error => onError(ws, error));
+//   console.log(`onConnection`);
+// }
+
+// function onConnection(ws, req) {
+//   console.log(`Web Socket Connected`)
+//   ws.on("message", (message) => {
+//     console.log(`Received: ${message}`)
+//     ws.broadcast(message)
+//   })
+//   ws.send(JSON.stringify({
+//     user: "Node",
+//     text: "Hello from Node.js Server"
+//   }))
+// }
+
+// function onConnection(ws) {
+//     ws.on('message', function incoming(message){
+//       var cliente
+//       console.log('received: ', message);
+//       ws.broadcast(message);
+//    });
+  
+//   ws.broadcast = function broadcast(msg){
+//     ws.clients.forEach(function each(client){
+//       client.send(msg);
+//     });
+//   };
+// }
 
 module.exports = (server) => {
   const wss = new WebSocket.Server({
     server
   });
 
-  wss.on('connection', onConnection);
+  // wss.on('connection', onConnection);
+
+  wss.on("connection", (ws) =>{
+    console.log(`Web Socket Connected ${wss.id}`)
+    ws.on('message', function incoming(message){
+      console.log('received: ', JSON.parse(message));
+      const mensagem = JSON.parse(message)
+      wss.broadcast(mensagem)
+    });
+  });
+
+  wss.broadcast = function broadcast(msg){
+    wss.clients.forEach(function each(client){
+      client.send(JSON.stringify(msg));
+    });
+  };
+
+  console.log("Server is liestening on port " + 3000)
   
   console.log(`App Web Socket Server is running!`);
 
